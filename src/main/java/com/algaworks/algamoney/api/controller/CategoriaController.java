03473bdaa3f49731df4b9aef9abafea6589400b7
George Bentes <george.bentes@gmail.com>
2019-08-17 19:30:55 -0400

    Add bean validation

---
+++ src/main/java/com/algaworks/algamoney/api/controller/CategoriaController.java
@@ -0,0 +1,53 @@
+package com.algaworks.algamoney.api.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.algaworks.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria>  listar(){
+		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria) {
+		Categoria categoriaSalva =  categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		
+		return this.categoriaRepository.findById(codigo)
+				.map(categoria -> ResponseEntity.ok(categoria))
+				.orElse(ResponseEntity.notFound().build());
+		
+	}
+	
+}
