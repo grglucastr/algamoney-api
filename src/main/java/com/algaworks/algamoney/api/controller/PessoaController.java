f65c21f9c5ab05203594a47f7339a054b59ce4c6
George Bentes <george.bentes@gmail.com>
2019-08-17 21:08:57 -0400

    Finish implementation of Pessoa resource

--- src/main/java/com/algaworks/algamoney/api/controller/PessoaController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/PessoaController.java
@@ -3,9 +3,12 @@ package com.algaworks.algamoney.api.controller;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,12 +20,24 @@ public class PessoaController {
 
     @GetMapping
     public List<Pessoa> listar(){
-        return null;
+        return pessoaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pessoa> pessoaPorId(@PathVariable Long id){
+        return pessoaRepository.findById(id)
+                .map(pessoa -> ResponseEntity.ok(pessoa))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Pessoa criarPessoa(@Valid @RequestBody Pessoa novaPessoa){
-        return null;
+    public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa novaPessoa){
+        Pessoa pessoa = pessoaRepository.save(novaPessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(pessoa.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(pessoa);
     }
 
 
--- src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
+++ src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull;
 public class Pessoa {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long Id;
 
     @NotNull
