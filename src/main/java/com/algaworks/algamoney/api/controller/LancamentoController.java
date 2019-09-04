96a248a7038924d6fe5127ecf9542ab758a20f6a
George Bentes <george.bentes@gmail.com>
2019-09-03 22:57:22 -0400

    Add get lancamento by id

--- src/main/java/com/algaworks/algamoney/api/controller/LancamentoController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/LancamentoController.java
@@ -5,6 +5,7 @@ import com.algaworks.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,17 @@ public class LancamentoController  {
     @GetMapping
     public List<Lancamento> listar(){
         return lancamentoRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Lancamento> lancamentoPorCodigo(@PathVariable Long codigo){
+
+        return lancamentoRepository
+                .findById(codigo)
+                .map(lancamento -> ResponseEntity.ok(lancamento))
+                .orElse(ResponseEntity.notFound().build());
+
+
     }
 
 
