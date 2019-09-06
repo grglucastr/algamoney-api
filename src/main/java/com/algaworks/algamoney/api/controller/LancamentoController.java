4373b5bc44320bb24375d33f19b922778ef946ac
George Bentes <george.bentes@gmail.com>
2019-09-05 20:51:12 -0400

    Add Filter

--- src/main/java/com/algaworks/algamoney/api/controller/LancamentoController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/LancamentoController.java
@@ -4,6 +4,7 @@ import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
 import com.algaworks.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler;
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
+import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
 import com.algaworks.algamoney.api.service.LancamentoService;
 import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,8 +41,10 @@ public class LancamentoController  {
     private MessageSource messageSource;
 
     @GetMapping
-    public List<Lancamento> listar(){
-        return lancamentoRepository.findAll();
+    public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter){
+
+        return lancamentoRepository.filtrar(lancamentoFilter);
+
     }
 
     @GetMapping("/{codigo}")
