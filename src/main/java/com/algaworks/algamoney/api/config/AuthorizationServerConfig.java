bed87ea5a92a4f79c8b36a129e50d4ef76707550
George Bentes <george.bentes@gmail.com>
2019-09-10 22:47:55 -0400

    Add ResourceServer and AuthorizationServer to generate token using the PasswordFlow strategy

---
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,46 @@
+package com.algaworks.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    // Responsável por obter: Usuário e Senha
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    // Configurar o cliente, neste caso, o Angular.
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("angular")
+                .secret("{noop}angular")
+                .scopes("read", "write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(1800);
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager); //validar com usuário e senha para ver se está tudo certo.
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+}
