41c63762cbe6a62a91528cc062995c34d19c48ad
George Bentes <george.bentes@gmail.com>
2019-09-18 19:43:07 -0400

    Add TokenEnhancer to add more information into accesstoken such as the user name

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.algaworks.algamoney.api.config;
 
+import com.algaworks.algamoney.api.config.token.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +10,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
@@ -45,11 +50,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
         endpoints
                 .tokenStore(tokenStore())
-                .accessTokenConverter(accessTokenConverter())
+                .tokenEnhancer(tokenEnhancerChain)
                 .reuseRefreshTokens(false)
                 .authenticationManager(authenticationManager); //validar com usuário e senha para ver se está tudo certo.
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
 
     @Bean
--- src/main/java/com/algaworks/algamoney/api/security/AppUserDetailsService.java
+++ src/main/java/com/algaworks/algamoney/api/security/AppUserDetailsService.java
@@ -27,7 +27,7 @@ public class AppUserDetailsService implements UserDetailsService {
         Optional<Usuario>usuarioOptional = usuarioRepository.findByEmail(email);
         Usuario usuario  = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuario e/ou senha incorretos."));
 
-        return new User(email, usuario.getSenha(), getPermissoes(usuario));
+        return new UsuarioSistema(usuario, getPermissoes(usuario));
 
     }
 
